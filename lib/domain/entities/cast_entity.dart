b722857d29322080ce6722402c6370681eccda4f
France! <franciscodomingosjose06798@gmail.com>
2024-04-23 16:30:28 +0100

    add first entities , cast and review

---
+++ lib/domain/entities/cast_entity.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+class CastEntity extends Equatable {
+  final String? name;
+  final String? profileUrl;
+  final int? gender;
+
+  const CastEntity({
+    required this.name,
+    required this.profileUrl,
+    required this.gender,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        profileUrl,
+        gender,
+      ];
+}
