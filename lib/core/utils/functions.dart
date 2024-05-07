be8edfcd3a177aa8d0f034dce7a5b2a58e922ecf
France! <franciscodomingosjose06798@gmail.com>
2024-05-07 10:57:11 +0100

    add readmore pack

--- lib/core/utils/functions.dart
+++ lib/core/utils/functions.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:my_cinema/core/data/network/api_constants.dart';
 import 'package:my_cinema/core/domain/entities/media.dart';
 import 'package:my_cinema/core/presentation/components/section_listview.dart';
+import 'package:my_cinema/core/presentation/components/section_listview_card.dart';
 import 'package:my_cinema/core/presentation/components/section_title.dart';
 import 'package:my_cinema/core/resources/app_colors.dart';
 import 'package:my_cinema/core/resources/app_routes.dart';
