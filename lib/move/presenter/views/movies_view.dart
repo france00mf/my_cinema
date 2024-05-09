0653c3b6781b23a5adc9dd7db3f60c74e7038f43
France! <franciscodomingosjose06798@gmail.com>
2024-05-09 17:19:33 +0100

    add MoviewsWidget and CustomSlider for carousel package adatper #CarouselSlider

--- lib/move/presenter/views/movies_view.dart
+++ lib/move/presenter/views/movies_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_cinema/core/domain/entities/media.dart';
+import 'package:my_cinema/core/presentation/components/custom_slider.dart';
+import 'package:my_cinema/core/presentation/components/slider_card.dart';
 
 
 class MoviesView extends StatelessWidget {
@@ -13,3 +16,37 @@ class MoviesView extends StatelessWidget {
     );
   }
 }
+
+class MoviesWidget extends StatelessWidget {
+  final List<Media> nowPlayingMovies;
+  final List<Media> popularMovies;
+  final List<Media> topRatedMovies;
+
+  const MoviesWidget({
+    super.key,
+    required this.nowPlayingMovies,
+    required this.popularMovies,
+    required this.topRatedMovies,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSlider(
+            itemBuilder: (context, itemIndex, _) {
+              return SliderCard(
+                media: nowPlayingMovies[itemIndex],
+                itemIndex: itemIndex,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
