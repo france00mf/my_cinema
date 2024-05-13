6d7ff5b3dc4ac7589478ab3b8d1543cc346298d7
France! <franciscodomingosjose06798@gmail.com>
2024-05-13 10:29:48 +0100

    add MoviesWidget on view to show items with bloc

--- lib/move/presenter/views/movies_view.dart
+++ lib/move/presenter/views/movies_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_cinema/core/domain/entities/media.dart';
 import 'package:my_cinema/core/presentation/components/custom_slider.dart';
 import 'package:my_cinema/core/presentation/components/slider_card.dart';
+import 'package:my_cinema/core/presentation/utils/enums.dart';
+import 'package:my_cinema/move/presenter/controllers/movies_bloc/movies_bloc.dart';
+
+import '../controllers/movies_bloc/movies_state.dart';
 
 
 class MoviesView extends StatelessWidget {
@@ -11,7 +16,19 @@ class MoviesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      body: Center(
+       body: BlocBuilder<MoviesBloc, MoviesState>(
+         builder: (context, state) {
+           switch (state.status) {
+              case RequestStatus.loading:
+                return const LinearProgressIndicator();
+              case RequestStatus.loaded:
+                return MoviesWidget(
+                  nowPlayingMovies: state.movies[0],
+                );
+              case RequestStatus.error:
+                return Scaffold(body: Center(child: Text("Erro"),),);
+            }
+         }
       ),
     );
   }
