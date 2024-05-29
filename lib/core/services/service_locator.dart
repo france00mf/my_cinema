833c16d9e37479c43569e90b74b59f5b6a2124a3
France! <franciscodomingosjose06798@gmail.com>
2024-05-29 11:03:47 +0100

    add popular injections on serviceLocator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -3,10 +3,12 @@ import 'package:my_cinema/move/data/datasource/move_datasource.dart';
 import 'package:my_cinema/move/data/datasource/move_datasource_imp.dart';
 import 'package:my_cinema/move/data/repository/movies_repository_impl.dart';
 import 'package:my_cinema/move/domain/repository/movies_repository.dart';
+import 'package:my_cinema/move/domain/usecases/get_all_popular_movies_usecase.dart';
 import 'package:my_cinema/move/domain/usecases/get_movie_details_usecase.dart';
 import 'package:my_cinema/move/domain/usecases/get_movies_usecase.dart';
 import 'package:my_cinema/move/presenter/controllers/movie_details_bloc/movie_details_bloc.dart';
 import 'package:my_cinema/move/presenter/controllers/movies_bloc/movies_bloc.dart';
+import 'package:my_cinema/move/presenter/controllers/popular_movies_bloc/popular_movies_bloc.dart';
 
 
 final sl = GetIt.instance;
@@ -26,10 +28,12 @@ class ServiceLocator {
       // Use Cases
       sl.registerLazySingleton(() => GetMoviesUseCase(sl()));
       sl.registerLazySingleton(() => GetMoviesDetailsUseCase(sl()));
+      sl.registerLazySingleton(() => GetAllPopularMoviesUseCase(sl()));
 
         // Bloc
     sl.registerFactory(() => MoviesBloc(sl()));
     sl.registerFactory(() => MovieDetailsBloc(sl()));
+    sl.registerFactory(() => PopularMoviesBloc(sl()));
 
   }
 }
