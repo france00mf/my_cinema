65f27ae6a51c2fae864b406cdd5ede72d507ee04
France! <franciscodomingosjose06798@gmail.com>
2024-05-30 10:31:31 +0100

    add rated topics on repository

--- lib/move/domain/repository/movies_repository.dart
+++ lib/move/domain/repository/movies_repository.dart
@@ -8,4 +8,5 @@ abstract class MoviesRespository {
   Future<Either<Failure, List<List<Media>>>> getMovies();
    Future<Either<Failure, MediaDetails>> getMovieDetails(int movieId);
    Future<Either<Failure, List<Media>>> getAllPopularMovies(int page);
+   Future<Either<Failure, List<Media>>> getAllTopRatedMovies(int page);
 }
