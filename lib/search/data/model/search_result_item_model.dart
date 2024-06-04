53b95a3a6ac18b3843f632409eb24cb784b13e94
France! <franciscodomingosjose06798@gmail.com>
2024-06-04 17:30:37 +0100

    fix error , add fromJson factory , but ain't finish

--- lib/search/data/model/search_result_item_model.dart
+++ lib/search/data/model/search_result_item_model.dart
@@ -3,5 +3,8 @@ import 'package:my_cinema/search/domain/entities/search_result_item_entity.dart'
 
 class SearchResultItemModel extends SearchResultItemEntity{
   SearchResultItemModel({required super.tmdbID, required super.posterUrl, required super.title, required super.isMovie});
+
+
+  factory SearchResultItemModel.fromJson(Map<String,dynamic> json)=> SearchResultItemModel(tmdbID: json[""], posterUrl: json[""], title: json[""], isMovie: json[""]);
   
 }
