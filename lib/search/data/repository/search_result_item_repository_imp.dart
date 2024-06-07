86011fb158b5dff3e8b9e2c730f78d86df89730c
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 13:10:30 +0100

    add Either on repositories

--- lib/search/data/repository/search_result_item_repository_imp.dart
+++ lib/search/data/repository/search_result_item_repository_imp.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_cinema/core/data/error/failure.dart';
 import 'package:my_cinema/search/data/model/search_result_item_model.dart';
 import 'package:my_cinema/search/domain/repository/search_result_item_repository.dart';
 
 class SearchResultItemImp implements SearchResultItemRepository {
   @override
-  Future<List<SearchResultItemModel>> search(String search) {
+  Future<Either<Failure, List<SearchResultItemModel>>> search(String search) {
     // TODO: implement search
     throw UnimplementedError();
   }
--- lib/search/domain/repository/search_result_item_repository.dart
+++ lib/search/domain/repository/search_result_item_repository.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_cinema/core/data/error/failure.dart';
 import 'package:my_cinema/search/data/model/search_result_item_model.dart';
 
 abstract class SearchResultItemRepository{
-  Future<List<SearchResultItemModel>> search(String search);
+  Future<Either<Failure, List<SearchResultItemModel>>> search(String search);
 }
