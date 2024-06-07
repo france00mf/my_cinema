a69bfb673c8b544ad5724004646a157459463548
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 13:07:35 +0100

    fix searchs repository and add minor changes

---
+++ lib/search/data/repository/search_result_item_repository_imp.dart
@@ -0,0 +1,11 @@
+import 'package:my_cinema/search/data/model/search_result_item_model.dart';
+import 'package:my_cinema/search/domain/repository/search_result_item_repository.dart';
+
+class SearchResultItemImp implements SearchResultItemRepository {
+  @override
+  Future<List<SearchResultItemModel>> search(String search) {
+    // TODO: implement search
+    throw UnimplementedError();
+  }
+
+}
--- lib/search/domain/repository/search_result_item_repository.dart
+++ lib/search/domain/repository/search_result_item_repository.dart
@@ -1,3 +1,5 @@
+import 'package:my_cinema/search/data/model/search_result_item_model.dart';
+
 abstract class SearchResultItemRepository{
-  
+  Future<List<SearchResultItemModel>> search(String search);
 }
