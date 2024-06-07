b3adc0e455836150f265c92d84e8e0862d667903
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 14:08:42 +0100

    init searchBloc implement

--- lib/search/presenter/controller/search_bloc/search_bloc.dart
+++ lib/search/presenter/controller/search_bloc/search_bloc.dart
@@ -1,66 +1,17 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_cinema/search/domain/use_case/search_result_item_use_case.dart';
 import 'package:my_cinema/search/presenter/controller/search_bloc/search_event.dart';
 import 'package:my_cinema/search/presenter/controller/search_bloc/search_state.dart';
 
-class SearchBloc implements Bloc<SearchEvent, SearchState>{
-  @override
-  void add(SearchEvent event) {
-    // TODO: implement add
+class SearchBloc extends Bloc<SearchEvent, SearchState>{
+  SearchBloc(this.baseSearchUseCase):super(const SearchState()){
+    on<GetSearchEvent>(_getSearchResults,transformer: deboundce(_duration));
+  };
+
+  final SearchResultItemUseCase baseSearchUseCase;
+
+  Future<void> _getSearchResults()async{
+
   }
-
-  @override
-  void addError(Object error, [StackTrace? stackTrace]) {
-    // TODO: implement addError
-  }
-
-  @override
-  Future<void> close() {
-    // TODO: implement close
-    throw UnimplementedError();
-  }
-
-  @override
-  void emit(SearchState state) {
-    // TODO: implement emit
-  }
-
-  @override
-  // TODO: implement isClosed
-  bool get isClosed => throw UnimplementedError();
-
-  @override
-  void on<E extends SearchEvent>(EventHandler<E, SearchState> handler, {EventTransformer<E>? transformer}) {
-    // TODO: implement on
-  }
-
-  @override
-  void onChange(Change<SearchState> change) {
-    // TODO: implement onChange
-  }
-
-  @override
-  void onError(Object error, StackTrace stackTrace) {
-    // TODO: implement onError
-  }
-
-  @override
-  void onEvent(SearchEvent event) {
-    // TODO: implement onEvent
-  }
-
-  @override
-  void onTransition(Transition<SearchEvent, SearchState> transition) {
-    // TODO: implement onTransition
-  }
-
-  @override
-  // TODO: implement state
-  SearchState get state => throw UnimplementedError();
-
-  @override
-  // TODO: implement stream
-  Stream<SearchState> get stream => throw UnimplementedError();
-
-
 }
