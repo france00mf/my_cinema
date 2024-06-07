c3f61323f9fa4e86c4fcb9d11500d6f7ee408f6d
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 13:58:36 +0100

    finesh searchState bloc

--- lib/search/presenter/controller/search_bloc/search_state.dart
+++ lib/search/presenter/controller/search_bloc/search_state.dart
@@ -1,8 +1,39 @@
 import 'package:equatable/equatable.dart';
+import 'package:my_cinema/search/domain/entities/search_result_item_entity.dart';
+
+
+enum SearchRequestStatus{empty, loading, loaded, error,noResults}
 
 class SearchState extends Equatable{
+  final List<SearchResultItemEntity> searchResults;
+  final SearchRequestStatus status;
+  final String message;
+
+  const SearchState(
+    {
+      this.searchResults = const[],
+      this.status = SearchRequestStatus.empty,
+      this.message = ''
+    }
+  );
+
+  SearchState copyWith({
+    List<SearchResultItemEntity>? searchResults,
+    SearchRequestStatus? status,
+    String? message
+  }){
+    return SearchState(
+      message: message ?? this.message,
+      searchResults: searchResults ?? this.searchResults ,
+      status:  status ?? this.status
+    );
+  }
   @override
   // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [
+        searchResults,
+        status,
+        message,
+  ];
   
 }
