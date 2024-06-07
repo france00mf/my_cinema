f88ce5ca131587f717ebf22b34af2880d56ac699
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 13:27:46 +0100

    add busnes logic on repository

--- lib/search/domain/use_case/search_result_item_use_case.dart
+++ lib/search/domain/use_case/search_result_item_use_case.dart
@@ -2,8 +2,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:my_cinema/core/data/error/failure.dart';
 import 'package:my_cinema/core/domain/usecase/base_use_case.dart';
+import 'package:my_cinema/search/domain/repository/search_result_item_repository.dart';
 
 class SearchResultItemUseCase implements BaseUseCase{
+  final SearchResultItemRepository baseSearchRepository;
+  SearchResultItemUseCase(this.baseSearchRepository);
+  
   @override
   Future<Either<Failure, dynamic>> call(p) {
     throw UnimplementedError();
