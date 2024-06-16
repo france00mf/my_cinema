463fdfeaa5b658272947ffd0119d98e7a7e19d02
France! <franciscodomingosjose06798@gmail.com>
2024-06-16 08:20:57 +0100

    create searchText and imp on searchView

---
+++ lib/search/presenter/view/components/search_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchText extends StatelessWidget {
+  const SearchText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme = Theme.of(context).textTheme;
+    return Expanded(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
