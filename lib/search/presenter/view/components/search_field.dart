572647e2edb0ed9a191efb02a4d7ce89f5ff55e6
France! <franciscodomingosjose06798@gmail.com>
2024-06-16 08:14:47 +0100

    add SearchField widget

---
+++ lib/search/presenter/view/components/search_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SearchField extends StatefulWidget {
+  const SearchField({super.key});
+
+  @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
