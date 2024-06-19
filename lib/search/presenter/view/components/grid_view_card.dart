9fe677291c87e166d9650763f318394ed1d61051
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 16:58:14 +0100

    fix errors on griViewCard

--- lib/search/presenter/view/components/grid_view_card.dart
+++ lib/search/presenter/view/components/grid_view_card.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_cinema/core/presentation/components/image_with_shimmer.dart';
+import 'package:my_cinema/core/resources/app_routes.dart';
 import 'package:my_cinema/core/resources/app_values.dart';
+import 'package:my_cinema/search/domain/entities/search_result_item_entity.dart';
 
 class GriViewCard extends StatelessWidget {
-  const GriViewCard({super.key, this.item});
-   final SearchResultItem item;
+  const GriViewCard({super.key,required  this.item});
+   final SearchResultItemEntity item;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +16,17 @@ class GriViewCard extends StatelessWidget {
       children: [
         GestureDetector(
           onTap: (){
-
+                    item.isMovie
+                ? context.pushNamed(AppRoutes.movieDetailsRoute,
+                    params: {'movieId': item.tmdbID.toString()})
+                : context.pushNamed(AppRoutes.tvShowDetailsRoute,
+                    params: {'tvShowId': item.tmdbID.toString()});
           },
           child: AspectRatio(aspectRatio: 2 /3, 
           
-          child: ClipRect(
-            borderRadius: BorderRadius.circular(AppSize.s8),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(AppSize.s8),
+              
                child: ImageWithShimmer(
                 imageUrl: item.posterUrl,
                 width: double.infinity,
--- lib/search/presenter/view/components/search_grid_view.dart
+++ lib/search/presenter/view/components/search_grid_view.dart
@@ -1,10 +1,12 @@
 
 import 'package:flutter/widgets.dart';
 import 'package:my_cinema/core/resources/app_values.dart';
+import 'package:my_cinema/search/domain/entities/search_result_item_entity.dart';
+import 'package:my_cinema/search/presenter/view/components/grid_view_card.dart';
 
 class SearchGridView extends StatelessWidget{
 
-  final List<SearchResultItem> results;
+  final List<SearchResultItemEntity> results;
 
   SearchGridView({super.key, required this.results});
 
@@ -14,7 +16,7 @@ class SearchGridView extends StatelessWidget{
     // TODO: implement build
     return Expanded(child: GridView.builder(
       itemCount: results.length,
-      itemBuilder: (context,index)=>GridViewCard(item: results[index]),
+      itemBuilder: (context,index)=> GriViewCard(item: results[index]),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 12, childAspectRatio: .55),
       physics: const BouncingScrollPhysics(),
       padding: const EdgeInsets.symmetric(vertical: AppPadding.p12),
@@ -26,5 +28,3 @@ class SearchGridView extends StatelessWidget{
 
 }
 
-class GridViewCard {
-}
