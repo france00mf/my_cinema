9b8dd6a535513be5d488afff2b3600a4765ef1ae
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 16:54:50 +0100

    finesh griViewCard

--- lib/search/presenter/view/components/grid_view_card.dart
+++ lib/search/presenter/view/components/grid_view_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_cinema/core/presentation/components/image_with_shimmer.dart';
+import 'package:my_cinema/core/resources/app_values.dart';
 
 class GriViewCard extends StatelessWidget {
   const GriViewCard({super.key, this.item});
@@ -6,6 +8,34 @@ class GriViewCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textTheme = Theme.of(context).textTheme;
+    return  Column(
+      children: [
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: AspectRatio(aspectRatio: 2 /3, 
+          
+          child: ClipRect(
+            borderRadius: BorderRadius.circular(AppSize.s8),
+               child: ImageWithShimmer(
+                imageUrl: item.posterUrl,
+                width: double.infinity,
+                height: AppSize.s150,
+              ),
+          ),),
+        ),
+
+               Expanded(
+          child: Text(
+            item.title,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: textTheme.bodyMedium,
+          ),
+        ),
+      ],
+    );
   }
 }
