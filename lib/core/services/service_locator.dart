80213a6cc54631a8c722ad935b5436d8006d8b34
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 17:16:51 +0100

    update

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -11,6 +11,12 @@ import 'package:my_cinema/move/presenter/controllers/movie_details_bloc/movie_de
 import 'package:my_cinema/move/presenter/controllers/movies_bloc/movies_bloc.dart';
 import 'package:my_cinema/move/presenter/controllers/popular_movies_bloc/popular_movies_bloc.dart';
 import 'package:my_cinema/move/presenter/controllers/top_rated_movies_bloc/top_rated_movies_bloc.dart';
+import 'package:my_cinema/search/data/datasource/search_datasource.dart';
+import 'package:my_cinema/search/data/datasource/search_datasource_imp.dart';
+import 'package:my_cinema/search/data/repository/search_result_item_repository_imp.dart';
+import 'package:my_cinema/search/domain/repository/search_result_item_repository.dart';
+import 'package:my_cinema/search/domain/use_case/search_result_item_use_case.dart';
+import 'package:my_cinema/search/presenter/controller/search_bloc/search_bloc.dart';
 
 
 final sl = GetIt.instance;
@@ -20,11 +26,15 @@ class ServiceLocator {
         // Data source
     sl.registerLazySingleton<MoviesRemoteDataSource>(
         () => MoviesRemoteDataSourceImpl());
+    
+    sl.registerLazySingleton<SearchDatasource>(() => SearchRemoteDataSourceImpl());
 
         
           // Repository
     sl.registerLazySingleton<MoviesRespository>(
         () => MoviesRepositoryImpl(sl()));
+
+    sl.registerLazySingleton<SearchResultItemRepository>(() => SearchResultItemRepositoryImp(sl()));
 
 
       // Use Cases
@@ -32,12 +42,14 @@ class ServiceLocator {
       sl.registerLazySingleton(() => GetMoviesDetailsUseCase(sl()));
       sl.registerLazySingleton(() => GetAllPopularMoviesUseCase(sl()));
       sl.registerLazySingleton(() => GetAllTopRatedMoviesUseCase(sl()));
+      sl.registerLazySingleton(() => SearchResultItemUseCase(sl()));
 
         // Bloc
     sl.registerFactory(() => MoviesBloc(sl()));
     sl.registerFactory(() => MovieDetailsBloc(sl()));
     sl.registerFactory(() => PopularMoviesBloc(sl()));
     sl.registerFactory(() => TopRatedMoviesBloc(sl()));
+    sl.registerFactory(() => SearchBloc(sl()));
 
   }
 }
--- lib/search/presenter/view/search_view.dart
+++ lib/search/presenter/view/search_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_cinema/core/presentation/components/error_text.dart';
 import 'package:my_cinema/core/presentation/components/loading_indicator.dart';
 import 'package:my_cinema/core/resources/app_values.dart';
+import 'package:my_cinema/core/services/service_locator.dart';
 import 'package:my_cinema/search/presenter/controller/search_bloc/search_bloc.dart';
 import 'package:my_cinema/search/presenter/controller/search_bloc/search_state.dart';
 import 'package:my_cinema/search/presenter/view/components/no_results.dart';
@@ -11,52 +12,60 @@ import 'package:my_cinema/search/presenter/view/components/search_field.dart';
 import 'package:my_cinema/search/presenter/view/components/search_grid_view.dart';
 import 'package:my_cinema/search/presenter/view/components/search_text.dart';
 
-class SearchView extends StatefulWidget {
+class SearchView extends StatelessWidget {
   const SearchView({super.key});
-
-  @override
-  State<SearchView> createState() => _SearchViewState();
-}
-
-class _SearchViewState extends State<SearchView> {
   
   @override
-  void initState() {
-    super.initState();  
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-             top: AppPadding.p12,
-            left: AppPadding.p16,
-            right: AppPadding.p16,
-        ),
-        child: Column(
-          children: [
-            SearchField(),
-            BlocBuilder<SearchBloc, SearchState>(
-              builder: (context, state) {
-                switch(state.status){
-                  case SearchRequestStatus.empty:
-                   return const SearchText();
-                  case SearchRequestStatus.loading:
-                    return const Expanded(child: LoadingIndicator());
-                  case SearchRequestStatus.loaded:
-                    return SearchGridView(results: state.searchResults);
-                  case SearchRequestStatus.error:
-                    return const Expanded(child: ErrorText());
-                  case SearchRequestStatus.noResults:
-                    return const NoResults();
-                }
-              },
-            )
-          ],
-        ),
-      ), 
+    // TODO: implement build
+    return BlocProvider(
+      create: (context)=>sl<SearchBloc>(),
+      child: SearchWidget(),
     );
   }
+
+
 }
 
+
+
+class SearchWidget extends StatelessWidget{
+  
+    SearchWidget({super.key});
+   @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(
+               top: AppPadding.p12,
+              left: AppPadding.p16,
+              right: AppPadding.p16,
+          ),
+          child: Column(
+            children: [
+              SearchField(),
+              BlocBuilder<SearchBloc, SearchState>(
+                builder: (context, state) {
+                  switch(state.status){
+                    case SearchRequestStatus.empty:
+                     return const SearchText();
+                    case SearchRequestStatus.loading:
+                      return const Expanded(child: LoadingIndicator());
+                    case SearchRequestStatus.loaded:
+                      return SearchGridView(results: state.searchResults);
+                    case SearchRequestStatus.error:
+                      return const Expanded(child: ErrorText());
+                    case SearchRequestStatus.noResults:
+                      return const NoResults();
+                  }
+                },
+              )
+            ],
+          ),
+        ), 
+      ),
+    );
+  }
+
+}
