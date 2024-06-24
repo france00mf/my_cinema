3c7ddab561132d7f727f2f7508311082aaab1d16
France! <franciscodomingosjose06798@gmail.com>
2024-06-24 15:18:14 +0100

    fixError on search a list of item

--- lib/search/domain/use_case/search_result_item_use_case.dart
+++ lib/search/domain/use_case/search_result_item_use_case.dart
@@ -2,14 +2,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:my_cinema/core/data/error/failure.dart';
 import 'package:my_cinema/core/domain/usecase/base_use_case.dart';
+import 'package:my_cinema/search/domain/entities/search_result_item_entity.dart';
 import 'package:my_cinema/search/domain/repository/search_result_item_repository.dart';
 
-class SearchResultItemUseCase implements BaseUseCase{
+class SearchResultItemUseCase implements BaseUseCase<List<SearchResultItemEntity>, String>{
   final SearchResultItemRepository baseSearchRepository;
   SearchResultItemUseCase(this.baseSearchRepository);
 
   @override
-  Future<Either<Failure, dynamic>> call(p) async{
+  Future<Either<Failure, List<SearchResultItemEntity>>> call(p) async{
     return await baseSearchRepository.search(p);
   }
 
