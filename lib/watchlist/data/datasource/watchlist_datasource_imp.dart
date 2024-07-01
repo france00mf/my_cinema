b8ad6ced1b4ef612123d746038aebfced0302f58
France! <franciscodomingosjose06798@gmail.com>
2024-07-01 21:06:10 +0100

    add getWatchListItems method contractor

--- lib/watchlist/data/datasource/watchlist_datasource_imp.dart
+++ lib/watchlist/data/datasource/watchlist_datasource_imp.dart
@@ -1,3 +1,5 @@
-class WatchListDatasourceImp{
-  
+import 'package:my_cinema/watchlist/data/model/watchlist_item_model.dart';
+
+abstract class WatchListDatasourceImp{
+  Future<List<WatchListItemModel>> getWatchListItems();
 }
