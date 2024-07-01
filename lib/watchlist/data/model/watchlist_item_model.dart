ea01afce4dadaa152ad087c8a134bb7c14cce3ec
France! <franciscodomingosjose06798@gmail.com>
2024-07-01 21:02:44 +0100

    extends watchlist model to media entitie then add factory convert

--- lib/watchlist/data/model/watchlist_item_model.dart
+++ lib/watchlist/data/model/watchlist_item_model.dart
@@ -1,3 +1,27 @@
-class WatchListItemModel{
-  
+import 'package:my_cinema/core/domain/entities/media.dart';
+
+class WatchListItemModel extends Media{
+    const WatchListItemModel({
+    required super.tmdbID,
+    required super.title,
+    required super.releaseDate,
+    required super.voteAverage,
+    required super.posterUrl,
+    required super.backdropUrl,
+    required super.overview,
+    required super.isMovie,
+  });
+
+  factory WatchListItemModel.fromEntity(Media media) {
+    return WatchListItemModel(
+      tmdbID: media.tmdbID,
+      title: media.title,
+      releaseDate: media.releaseDate,
+      voteAverage: media.voteAverage,
+      posterUrl: media.posterUrl,
+      backdropUrl: media.backdropUrl,
+      overview: media.overview,
+      isMovie: media.isMovie,
+    );
+  }
 }
