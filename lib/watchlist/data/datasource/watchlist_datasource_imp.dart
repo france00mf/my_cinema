6ce62ad9e0f10af31866f2f3808b653e6f9a91a9
France! <franciscodomingosjose06798@gmail.com>
2024-07-01 21:08:04 +0100

    create addWatchList , removeWatchList and isItemAdded method datasource contractor

--- lib/watchlist/data/datasource/watchlist_datasource_imp.dart
+++ lib/watchlist/data/datasource/watchlist_datasource_imp.dart
@@ -2,4 +2,7 @@ import 'package:my_cinema/watchlist/data/model/watchlist_item_model.dart';
 
 abstract class WatchListDatasourceImp{
   Future<List<WatchListItemModel>> getWatchListItems();
+  Future<int> addWatchListItem(WatchListItemModel item);
+  Future<void> removeWatchListItem(int index);
+  Future<int> isItemAdded(int tmdbId);
 }
