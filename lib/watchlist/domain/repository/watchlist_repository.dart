a7faf4d47378d4c16cb467a6155f9204da91a2ff
France! <franciscodomingosjose06798@gmail.com>
2024-07-01 21:00:01 +0100

    create, addWatchListItem, removeWatchListItem and checkItemAdded repositoty abstract

--- lib/watchlist/domain/repository/watchlist_repository.dart
+++ lib/watchlist/domain/repository/watchlist_repository.dart
@@ -1,4 +1,11 @@
 
+import 'package:dartz/dartz.dart';
+import 'package:my_cinema/core/data/error/failure.dart';
+import 'package:my_cinema/core/domain/entities/media.dart';
+
 abstract class WatchlistRepository{
-  Future<Either<Failure, List<Media>> getWatchListItems();
+  Future<Either<Failure, List<Media>>> getWatchListItems();
+  Future<Either<Failure, int>> addWatchListItem(Media media);
+  Future<Either<Failure, Unit>> removeWatchListItem(int index);
+  Future<Either<Failure, int>> checkIfItemAdded(int tmdbId);
 }
