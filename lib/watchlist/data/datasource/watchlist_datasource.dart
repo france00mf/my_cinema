f72248a258770be999eb2b21a1f3877e00a6a84a
France! <franciscodomingosjose06798@gmail.com>
2024-07-12 12:39:19 +0100

    fix import of watchList

--- lib/watchlist/data/datasource/watchlist_datasource.dart
+++ lib/watchlist/data/datasource/watchlist_datasource.dart
@@ -1,3 +1,9 @@
+import 'package:my_cinema/watchlist/data/model/watchlist_item_model.dart';
+
+
 class WatchListDatasource{
-  
+    Future<List<WatchListItemModel>> getWatchListItems();
+  Future<int> addWatchListItem(WatchListItemModel item);
+  Future<void> removeWatchListItem(int index);
+  Future<int> isItemAdded(int tmdbId);
 }
--- lib/watchlist/data/datasource/watchlist_datasource_imp.dart
+++ lib/watchlist/data/datasource/watchlist_datasource_imp.dart
@@ -1,8 +1,5 @@
 import 'package:my_cinema/watchlist/data/model/watchlist_item_model.dart';
 
-abstract class WatchListDatasourceImp{
-  Future<List<WatchListItemModel>> getWatchListItems();
-  Future<int> addWatchListItem(WatchListItemModel item);
-  Future<void> removeWatchListItem(int index);
-  Future<int> isItemAdded(int tmdbId);
+class WatchListDatasourceImp implements WatchListDatasource{
+ 
 }
