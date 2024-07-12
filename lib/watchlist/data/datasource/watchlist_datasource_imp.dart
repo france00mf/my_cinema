a065dd3e85880245051d7bda94ca6a93d2885790
France! <franciscodomingosjose06798@gmail.com>
2024-07-12 12:40:11 +0100

    implement watchlist

--- lib/watchlist/data/datasource/watchlist_datasource_imp.dart
+++ lib/watchlist/data/datasource/watchlist_datasource_imp.dart
@@ -1,5 +1,29 @@
+import 'package:my_cinema/watchlist/data/datasource/watchlist_datasource.dart';
 import 'package:my_cinema/watchlist/data/model/watchlist_item_model.dart';
 
 class WatchListDatasourceImp implements WatchListDatasource{
+  @override
+  Future<int> addWatchListItem(WatchListItemModel item) {
+    // TODO: implement addWatchListItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<WatchListItemModel>> getWatchListItems() {
+    // TODO: implement getWatchListItems
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> isItemAdded(int tmdbId) {
+    // TODO: implement isItemAdded
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeWatchListItem(int index) {
+    // TODO: implement removeWatchListItem
+    throw UnimplementedError();
+  }
  
 }
